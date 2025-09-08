e4411853991185e9605bba66b2ef936c6b1cb434
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-08 10:46:08 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,29 @@
+/* menu.js */
+$(document).ready(function() {
+    // Cargar submenús
+    $("#entrenadores-submenu").load("submenus.html #entrenadores-submenu");
+    $("#porteros-submenu").load("submenus.html #porteros-submenu");
+    $("#defensas-submenu").load("submenus.html #defensas-submenu");
+    $("#centrocampistas-submenu").load("submenus.html #centrocampistas-submenu");
+    $("#delanteros-submenu").load("submenus.html #delanteros-submenu");
+    $("#alineaciones-submenu").load("submenus.html #alineaciones-submenu");
 
+    // Botón hamburguesa
+    $("#bun").click(function() {
+        $(this).toggleClass("menu-open");
+        $("#sidebar").toggleClass("nav-slide");
+        $(".menu-header-image").toggleClass("show");
+        $(".nav-item").each(function(i) {
+            var item = $(this);
+            setTimeout(function() {
+                item.toggleClass("item-slide");
+            }, i * 50);
+        });
+    });
+
+    // Submenús
+    $('.nav-item.has-submenu').click(function() {
+        $(this).find('.sub-nav').slideToggle();
+        $(this).toggleClass('selected');
+    });
+});
