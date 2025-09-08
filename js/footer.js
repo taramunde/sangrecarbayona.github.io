07391eea44ad9afa64a56eeb5649f305775fb5ac
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-08 12:42:48 +0200

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function() {
+    // Cargar el footer dinámicamente
+    $('#footer-placeholder').load('footer.html', function() {
+        // Actualizar el año actual dinámicamente después de cargar el footer
+        const currentYear = new Date().getFullYear();
+        $('#current-year').text(currentYear);
+    });
+});
