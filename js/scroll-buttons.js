0b1711c275d662f506e304cd50e0fa9823ca4e0c
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-08 14:13:14 +0200

    Update scroll-buttons.js

--- js/scroll-buttons.js
+++ js/scroll-buttons.js
@@ -1,1 +1,6 @@
-
+document.getElementById('scroll-up').onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+document.getElementById('scroll-down').onclick = function() {
+  window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+};
