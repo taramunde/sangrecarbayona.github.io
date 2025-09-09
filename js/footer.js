6cc5f24655283cd206b2eabbddab90f460102022
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-09 20:20:23 +0200

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,6 @@ document.getElementById('footer-placeholder').innerHTML = `
     <div class="footer-container">
         <div class="footer-content">
             <p>© 2025 Sangre Carbayona. Todos los derechos reservados.</p>
-            <p>Contacto: info@sangrecarbayona.com | Síguenos en <a href="https://twitter.com/sangrecarbayona">X</a></p>
-        </div>
+         </div>
     </div>
 `;
