b8aeaa86ab4e6f08fc1321f96f8d855b9ffdae97
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-11 05:06:38 +0200

    Delete js/blinking-sign.js

--- js/blinking-sign.js
+++
@@ -1,8 +0,0 @@
-window.setInterval(BlinkIt, 500);
-var color = "blue";
-function BlinkIt() {
-    var blink = document.getElementById("blink");
-    color = (color == "#ffffff") ? "blue" : "#ffffff";
-    blink.style.color = color;
-    blink.style.fontSize = '36px';
-}
