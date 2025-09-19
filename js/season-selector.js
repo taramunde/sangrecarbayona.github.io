3944ece240a4703caad8489a671bb2ebb3294cd4
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-19 14:06:45 +0200

    Update season-selector.js

--- js/season-selector.js
+++ js/season-selector.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const seasonButton = document.getElementById('season-button');
-    const seasonContainer = document.getElementById('season-container');
-    const closeButton = document.getElementById('close-button');
+ const seasonButton = document.getElementById('season-button');
+ const seasonContainer = document.getElementById('season-container');
+ const closeButton = document.getElementById('close-button');
 
-    seasonButton.addEventListener('click', function() {
-        const isOpen = seasonContainer.classList.contains('open');
-        if (isOpen) {
-            seasonContainer.classList.remove('open');
-            seasonButton.setAttribute('aria-expanded', 'false');
-        } else {
-            seasonContainer.classList.add('open');
-            seasonButton.setAttribute('aria-expanded', 'true');
-        }
-    });
+ seasonButton.addEventListener('click', function() {
+ const isOpen = seasonContainer.classList.contains('open');
+ if (isOpen) {
+ seasonContainer.classList.remove('open');
+ closeButton.classList.remove('open');
+ seasonButton.setAttribute('aria-expanded', 'false');
+ } else {
+ seasonContainer.classList.add('open');
+ closeButton.classList.add('open');
+ seasonButton.setAttribute('aria-expanded', 'true');
+ }
+ });
 
-    closeButton.addEventListener('click', function() {
-        seasonContainer.classList.remove('open');
-        seasonButton.setAttribute('aria-expanded', 'false');
-    });
+ closeButton.addEventListener('click', function() {
+ seasonContainer.classList.remove('open');
+ closeButton.classList.remove('open');
+ seasonButton.setAttribute('aria-expanded', 'false');
+ });
 });
