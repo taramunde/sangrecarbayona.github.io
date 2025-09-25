c91245de7889b35c50d0eaa45c8a9c8c5228781d
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-25 17:06:06 +0200

    Update players.js

--- js/players.js
+++ js/players.js
@@ -152,7 +152,8 @@ const allPlayersData = {
             { src: "https://i.postimg.cc/R0tYw3y8/Masca-PNG.webp", alt: "Delantero 4", number: "20", name: "Masca", url: "#" },
             { src: "https://i.postimg.cc/R02PFM0R/Dubasin-PNG.webp", alt: "Delantero 5", number: "22", name: "Dubasin", url: "#" },
             { src: "https://i.postimg.cc/TYQCbjwh/Enol-PNG.webp", alt: "Delantero 6", number: "27", name: "Enol", url: "#" },
-            { src: "https://i.postimg.cc/2y305Pxm/Ses-PNG.webp", alt: "Delantero 7", number: "28", name: "Sesé", url: "#" }
+            { src: "https://i.postimg.cc/2y305Pxm/Ses-PNG.webp", alt: "Delantero 7", number: "28", name: "Sesé", url: "#" },
+            { src: "https://i.postimg.cc/kXjLCPWj/Mario-Fuente-PNG.webp", alt: "Delantero 8", number: "-", name: "Mario Fuente", url: "#" }
         ],
         coaches: [
             { src: "https://i.postimg.cc/fLnY0kQ8/Javi-Calleja-entrenador-PNG.webp", alt: "Entrenador 1", number: "-", name: "Javi Calleja", url: "#" },
