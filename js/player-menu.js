c49d93484bdffd8d70ece5eef4463aa99127eea4
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-28 18:03:36 +0200

    Update player-menu.js

--- js/player-menu.js
+++ js/player-menu.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+    // Manejar clics en las pestañas
+    $('.menu-tab').on('click', function() {
+        // Remover clase active de todas las pestañas y contenidos
+        $('.menu-tab').removeClass('active');
+        $('.tab-content').removeClass('active');
 
+        // Añadir clase active al elemento clicado y su contenido
+        $(this).addClass('active');
+        $('#' + $(this).data('tab')).addClass('active');
+    });
+
+    // Por defecto, activar Estadísticas
+    $('.menu-tab[data-tab="stats"]').addClass('active');
+    $('#stats').addClass('active');
+});
