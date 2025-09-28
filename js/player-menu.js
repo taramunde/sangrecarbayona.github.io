b521e9441ae767d3403eeb3dc1f760558d5b923c
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-28 18:48:44 +0200

    Update player-menu.js

--- js/player-menu.js
+++ js/player-menu.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-    // Manejar clics en las pestañas
+    // Manejar clics en las pestañas principales
     $('.menu-tab').on('click', function() {
-        // Remover clase active de todas las pestañas y contenidos
         $('.menu-tab').removeClass('active');
         $('.tab-content').removeClass('active');
-
-        // Añadir clase active al elemento clicado y su contenido
         $(this).addClass('active');
         $('#' + $(this).data('tab')).addClass('active');
     });
@@ -13,4 +10,23 @@ $(document).ready(function() {
     // Por defecto, activar Estadísticas
     $('.menu-tab[data-tab="stats"]').addClass('active');
     $('#stats').addClass('active');
+
+    // Manejar clics en los botones desplegables
+    $('.dropdown-btn').on('click', function() {
+        const menu = $(this).next('.dropdown-menu');
+        $('.dropdown-menu').not(menu).hide(); // Cierra otros menús abiertos
+        menu.toggle(); // Muestra/oculta el menú correspondiente
+    });
+
+    // Manejar clics en el botón de cerrar
+    $('.close-btn').on('click', function() {
+        $(this).parent('.dropdown-menu').hide();
+    });
+
+    // Cerrar menús si se hace clic fuera
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.stats-buttons').length) {
+            $('.dropdown-menu').hide();
+        }
+    });
 });
