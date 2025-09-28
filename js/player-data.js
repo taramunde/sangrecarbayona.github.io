0912dcbcdfe8cda6c2bf7cd76d62daec88c883fc
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-29 01:41:01 +0200

    Update player-data.js

--- js/player-data.js
+++ js/player-data.js
@@ -70,15 +70,14 @@ const playersData = {
         number: 2,
         image: "https://i.postimg.cc/qRHtqnX7/Bailly-PNG-2.webp", 
         stats: {
-            "2025/26": {
-                liga: { partidos: 1, goles_marcados: 0, tarjetas_amarillas: 1, tarjetas_rojas: 0 },
-                copa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
-                uefa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
-                amistosos: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
+            "2024/25": {
+                liga: { partidos: 0, goles_encajados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
+                copa: { partidos: 0, goles_encajados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
+                uefa: { partidos: 0, goles_encajados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
+                amistosos: { partidos: 0, goles_encajados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
             }
-            
         }
-     }
+    },
 
     
     
