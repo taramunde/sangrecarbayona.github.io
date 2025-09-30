ef45ba4d28c435a6a6a38a02a8f502d81c8d842c
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-30 23:48:13 +0200

    Update player-data.js

--- js/player-data.js
+++ js/player-data.js
@@ -212,7 +212,7 @@ const playersData = {
         image: "https://i.postimg.cc/15bMB7y7/Rahim-PNG.webp", 
         stats: {
             "2025/26": {
-                liga: { partidos: 6, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
+                liga: { partidos: 7, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 copa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 uefa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 amistosos: { partidos: 4, goles_encajados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
