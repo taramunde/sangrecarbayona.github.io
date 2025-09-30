0c909e1a48e1c33c02daa80c77b592e40b6b3ff5
taramunde <63210114+taramunde@users.noreply.github.com>
2025-09-30 11:01:14 +0200

    Update player-data.js

--- js/player-data.js
+++ js/player-data.js
@@ -1,4 +1,5 @@
 const playersData = {
+    // PORTEROS
     "aaron": {
         name: "Aarón Escandell Banacloche",
         footballName: "Aarón",
@@ -6,7 +7,7 @@ const playersData = {
         role: "portero",
         number: 13,
         image: "https://i.postimg.cc/hvxDQPg1/Aar-n-PNG-3.webp",
-        career: [  // NUEVO CAMPO
+        career: [  
             { season: "2025/26", team: "Real Oviedo", category: "1ª Div." },
             { season: "2024/25", team: "Real Oviedo", category: "2ª Div. - 2ª Play Offs Ascenso" },
             { season: "2023/24", team: "U.D. Las Palmas", category: "1ª Div. - Copa del Rey" },
@@ -48,7 +49,7 @@ const playersData = {
         role: "portero",
         number: 1,
         image: "https://i.postimg.cc/sgPM0mk4/Moldovan-PNG-2.webp", 
-        career: [  // NUEVO CAMPO
+        career: [  
             { season: "2025/26", team: "Real Oviedo", category: "1ª División" },
             { season: "2024/25", team: "U.S. Sassuolo", category: "Serie B - Coppa Italia" },
             { season: "2024/25", team: "Atlético de Madrid", category: "1ª División" },
@@ -90,7 +91,7 @@ const playersData = {
         role: "portero",
         number: 26,
         image: "https://i.postimg.cc/XJqXqbps/Narv-ez-PNG.webp", 
-        career: [  // NUEVO CAMPO
+        career: [  
             { season: "2025/26", team: "Real Oviedo Vetusta", category: "2ª Federación" },
             { season: "2025/26", team: "Real Oviedo", category: "1ª Div." },
             { season: "2024/25", team: "Real Oviedo Vetusta", category: "3ª Federación" },
@@ -103,8 +104,6 @@ const playersData = {
             { season: "2020/21", team: "C.D. Badajoz ", category: "2ª Div. B - 2ª Div. B Play Offs Ascenso - 2ª Div. B 2ª Fase Ascenso a 2ª" },
             { season: "2020/21", team: "C.D. Badajoz B", category: "1ª Reg. Extremadura" },
             { season: "2019/20", team: "C.D. Badajoz B", category: "1ª Reg. Extremadura" }
-            
-            
         ],
         stats: {
             "2025/26": {
@@ -121,7 +120,7 @@ const playersData = {
             }
         }
     },
-
+    // DEFENSAS
     "bailly": {
         name: "Eric Bertrand Bailly",
         footballName: "Bailly",
@@ -129,6 +128,40 @@ const playersData = {
         role: "jugador",
         number: 2,
         image: "https://i.postimg.cc/qRHtqnX7/Bailly-PNG-2.webp", 
+        career: [  
+            { season: "2025/26", team: "Real Oviedo", category: "1ª Div." },
+            { season: "2024/25", team: "Villarreal C.F.", category: "1ª Div. - Copa del Rey" },
+            { season: "2023/24", team: "Villarreal C.F.", category: "1ª Div. - Europa League - Copa del Rey" },
+            { season: "2023/24", team: "Beşiktaş J.K.", category: "Liga Turca - Conference League" },
+            { season: "2022/23", team: "Olympique Marseille", category: "Ligue 1 - Champions League - Copa de Francia" },
+            { season: "2021/22", team: "Manchester United", category: "Premier League - Champions League - EFL Cup" },
+            { season: "2020/21", team: "Manchester United", category: "Premier League - Champions League - Europa League - FA Cup - EFL Cup" },
+            { season: "2019/20", team: "Manchester United", category: "Premier League - Europa League - FA Cup - EFL Cup" },
+            { season: "2019/20", team: "Manchester United U23", category: "Premier League 2ª Div. Two" },
+            { season: "2018/19", team: "Manchester United ", category: "Premier League - Champions League - FA Cup - EFL Cup" },
+            { season: "2017/18", team: "Manchester United", category: "Premier League - Champions League - FA Cup" },
+            { season: "2016/17", team: "Manchester United U23", category: "Premier League 2ª Div. One" },
+            { season: "2016/17", team: "Manchester United", category: "Premier League - Europa League - FA Cup - Community Shield - EFL Cup" },
+            { season: "2015/16", team: "Villarreal C.F.", category: "1ª Div. - Europa League - Copa del Rey" },
+            { season: "2014/15", team: "Villarreal C.F.", category: "1ª Div. - Europa League - Copa del Rey" },
+            { season: "2014/15", team: "R.C.D. Espanyol", category: "1ª Div. - Copa del Rey" },
+            { season: "2014/15", team: "R.C.D. Espanyol B", category: "2ª Div. B" },
+            { season: "2013/14", team: "R.C.D. Espanyol B", category: "2ª Div. B" },
+            { season: "2012/13", team: "R.C.D. Espanyol B", category: "2ª Div. B" }
+        ],
+        nationalTeam: [
+            { season: "2024", team: "Costa de Marfil", category: "Clasificación Copa África" },
+            { season: "2023", team: "Costa de Marfil", category: "Clasificación Copa África" },
+            { season: "2022", team: "Costa de Marfil", category: "Copa África - Amistosos" },
+            { season: "2021", team: "Costa de Marfil Sub-23", category: "Juegos Olímpicos" },
+            { season: "2021", team: "Costa de Marfil", category: "Clasificación Mundial África - Clasificación Copa África - Amistosos" },
+            { season: "2020", team: "Costa de Marfil", category: "Amistosos" },
+            { season: "2019", team: "Costa de Marfil", category: "Clasificación Copa África" },
+            { season: "2018", team: "Costa de Marfil", category: "Clasificación Copa África - Amistosos" },
+            { season: "2017", team: "Costa de Marfil", category: "Clasificación Mundial África - Copa África - Clasificación Copa África - Amistosos" },
+            { season: "2016", team: "Costa de Marfil", category: "Clasificación Mundial África - Clasificación Copa África - Amistosos" },
+            { season: "2015", team: "Costa de Marfil", category: "Clasificación Mundial África - Copa África - Clasificación Copa África - Amistosos" }
+        ],
         stats: {
             "2025/26": {
                 liga: { partidos: 1, goles_marcados: 0, tarjetas_amarillas: 1, tarjetas_rojas: 0 },
