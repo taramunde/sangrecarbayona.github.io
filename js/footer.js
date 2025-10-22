92ef89f5ae4782f748270b39b38aa2649e31da9d
taramunde <63210114+taramunde@users.noreply.github.com>
2025-10-22 15:07:29 +0200

    Delete js directory

--- js/footer.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('footer-placeholder').innerHTML = `
-    <div class="footer-container">
-        <div class="footer-content">
-            <p>© 2025 Sangre Carbayona. Todos los derechos reservados.</p>
-         </div>
-    </div>
-`;
