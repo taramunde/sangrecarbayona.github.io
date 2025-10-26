cdd945c0aeb3e1800ae58a12d51bf0ff84b892cc
taramunde <63210114+taramunde@users.noreply.github.com>
2025-10-26 18:20:04 +0100

    Update players-database.js

--- js/players-database.js
+++ js/players-database.js
@@ -1,1 +1,114 @@
+// Base de datos única de todos los jugadores del Real Oviedo
 
+const playersDatabase = {
+    // PORTEROS
+    "aaron": {
+        "src": "https://i.postimg.cc/hvxDQPg1/Aar-n-PNG-3.webp",
+        "alt": "Portero",
+        "number": "-",
+        "name": "Aarón",
+        "url": "fichajugador.html?player=aaron"
+    },
+    "moldovan": {
+        "src": "https://i.postimg.cc/sgPM0mk4/Moldovan-PNG-2.webp",
+        "alt": "Portero",
+        "number": "-",
+        "name": "Moldovan",
+        "url": "fichajugador.html?player=moldovan"
+    },
+    "narvaez": {
+        "src": "https://i.postimg.cc/XJqXqbps/Narv-ez-PNG.webp",
+        "alt": "Portero",
+        "number": "-",
+        "name": "Narváez",
+        "url": "fichajugador.html?player=narvaez"
+    },
+    "quentinbraat": {
+        "src": "https://i.postimg.cc/wxsLQXj0/Quentin-Braat-PNG.webp",
+        "alt": "Portero",
+        "number": "-",
+        "name": "Quentin Braat",
+        "url": "fichajugador.html?player=quentinbraat"
+    },
+    "viti": {
+        "src": "https://i.postimg.cc/L625gLYq/Viti_PNG.webp",
+        "alt": "Portero",
+        "number": "-",
+        "name": "Viti",
+        "url": "fichajugador.html?player=viti"
+    },
+    
+    // DEFENSAS
+    "bailly": {
+        "src": "https://i.postimg.cc/qRHtqnX7/Bailly-PNG-2.webp",
+        "alt": "Defensa",
+        "number": "-",
+        "name": "Bailly",
+        "url": "fichajugador.html?player=bailly"
+    },
+    "davidcostas": {
+        "src": "https://i.postimg.cc/V6ScxHbr/David-Costas-PNG.webp",
+        "alt": "Defensa",
+        "number": "-",
+        "name": "David Costas",
+        "url": "fichajugador.html?player=davidcostas"
+    },
+    "danicalvo": {
+        "src": "https://i.postimg.cc/Wb0spKY0/Dani-Calvo-PNG.webp",
+        "alt": "Defensa",
+        "number": "-",
+        "name": "Dani Calvo",
+        "url": "fichajugador.html?player=danicalvo"
+    },
+    "luengo": {
+        "src": "https://i.postimg.cc/YqQmcd1s/Luengo-PNG.webp",
+        "alt": "Defensa",
+        "number": "-",
+        "name": "Luengo",
+        "url": "fichajugador.html?player=luengo"
+    },
+    
+    // CENTROCAMPISTAS
+    "santicazorla": {
+        "src": "https://i.postimg.cc/6qQmv7tk/Santi-Cazorla-PNG.webp",
+        "alt": "Centrocampista",
+        "number": "-",
+        "name": "Santi Cazorla",
+        "url": "fichajugador.html?player=santicazorla"
+    },
+    "borjasanchez": {
+        "src": "https://i.postimg.cc/v82kfvkD/Borja-S-nchez-PNG.webp",
+        "alt": "Centrocampista",
+        "number": "-",
+        "name": "Borja Sánchez",
+        "url": "fichajugador.html?player=borjasanchez"
+    },
+    
+    // DELANTEROS
+    "oli": {
+        "src": "https://i.postimg.cc/fbMdfprN/Oli_PNG.webp",
+        "alt": "Delantero",
+        "number": "-",
+        "name": "Oli",
+        "url": "fichajugador.html?player=oli"
+    },
+    
+    // ENTRENADORES
+    "paunovic": {
+        "src": "https://i.postimg.cc/9FK0fKhp/PaunoviÄ‡_(3)_(entrenador).webp",
+        "alt": "Entrenador",
+        "number": "-",
+        "name": "Paunović",
+        "url": "fichajugador.html?player=paunovic"
+    },
+    "luiscarrion": {
+        "src": "https://i.postimg.cc/9fT7VVwZ/Luis-Carri-n-entrenador-PNG.webp",
+        "alt": "Entrenador",
+        "number": "-",
+        "name": "Luis Carrión",
+        "url": "fichajugador.html?player=luiscarrion"
+    }
+    
+    // AQUÍ AÑADES TODOS LOS DEMÁS JUGADORES DE TU HISTORIAL
+    // Solo una vez cada uno, sin repetir
+};
