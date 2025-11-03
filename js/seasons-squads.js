93e0ac07bf357610a9d36f1278c719063ec8fd35
taramunde <63210114+taramunde@users.noreply.github.com>
2025-11-03 05:02:47 +0100

    Add seasonsSquads object with player data

--- js/seasons-squads.js
+++ js/seasons-squads.js
@@ -1,1 +1,12 @@
-
+const seasonsSquads = {
+    "2025-26": {
+        "goalkeepers": ["aaron", "moldovan", "narvaez"],
+        "defenders": ["bailly", "rahim", "davidcostas"],
+        // ... solo IDs
+    },
+    "2024-25": {
+        "goalkeepers": ["quentinbraat", "aaron", "narvaez"],
+        // ... solo IDs
+    }
+    // ... todas las temporadas
+};
