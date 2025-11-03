8f4677869aa3fe3f938b21859eb338ac00f1819a
taramunde <63210114+taramunde@users.noreply.github.com>
2025-11-03 16:12:10 +0100

    Update seasons-squads.js

--- js/seasons-squads.js
+++ js/seasons-squads.js
@@ -2,13 +2,17 @@ const seasonsSquads = {
     "2025-26": {
         "goalkeepers": ["aaron", "moldovan", "narvaez"],
         "defenders": ["bailly", "rahim", "davidcostas", "danicalvo", "luengo", "davidcarmo", "nachovidal", "lucas", "javilopez", "alvarolemos", "lamine", "omarfalah", "marcoesteban", "adrilopes"],
-        "midfielders": ["bailly", "rahim", "davidcostas"],
-        "forwards": ["bailly", "rahim", "davidcostas"],
+        "midfielders": ["albertoreina", "sibo", "santicazorla", "hassan", "colombatto", "ejaria", "borjasanchez", "brandondomingues", "brekalo", "dendoncker", "ilic", "alexcardero", "cheli", "diegotejon", "dieguito", "agudin"],
+        "forwards": ["ilyas", "fedevinas", "alexfores", "rondon"],
         "coaches": ["paunovic", "luiscarrion"],
         // ... solo IDs
     },
     "2024-25": {
         "goalkeepers": ["quentinbraat", "aaron", "narvaez"],
+        "defenders": ["alvarolemos", "rahim", "davidcostas", "nachovidal", "danicalvo", "luengo", "pomares", "lucas", "jaimevazquez", "marcoesteban", "eze", "adrilopes", "omarfalah", "lamine"],
+        "midfielders": ["albertodelmoral", "sibo", "sebasmoyano", "santicazorla", "borjasanchez", "portillo", "colombatto", "delahoz", "paulino2024", "seoane2024", "dotor", "hassan", "cheli", "alexcardero", "valdera", "diegotejon", "castri"],
+        "forwards": ["alemao", "paraschiv", "ilyas", "masca", "alexmillan", "fedevinas", "santimiguelez"],
+        "coaches": ["javicalleja", "paunovic"],
         // ... solo IDs
     }
     // ... todas las temporadas
