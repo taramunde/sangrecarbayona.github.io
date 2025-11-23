3c8e1139c89ff312f5fcf14fe74e7fe881ff82fa
taramunde <63210114+taramunde@users.noreply.github.com>
2025-11-23 21:37:36 +0100

    Update clasificacion.js

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -179,7 +179,7 @@ const enfrentamientos = [
   { equipo1: "C.A. Osasuna", equipo2: "Real Sociedad", goles1: 1, goles2: 3 },
   { equipo1: "Real Oviedo", equipo2: "Rayo Vallecano", goles1: 0, goles2: 0 },
   { equipo1: "F.C. Barcelona", equipo2: "Athletic Club", goles1: 4, goles2: 0 },
-  { equipo1: "Getafe C.F.", equipo2: "Atlético de Madrid", goles1: null, goles2: null },
+  { equipo1: "Getafe C.F.", equipo2: "Atlético de Madrid", goles1: 0, goles2: 1 },
   { equipo1: "Valencia C.F.", equipo2: "Levante U.D.", goles1: 1, goles2: 0 },
   { equipo1: "Villarreal C.F.", equipo2: "R.C.D. Mallorca", goles1: 2, goles2: 1 },
   
