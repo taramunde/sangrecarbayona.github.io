9521369a59c4312f38314a3099b7a61d4a90de94
taramunde <63210114+taramunde@users.noreply.github.com>
2025-11-23 16:25:13 +0100

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -7,80 +7,29 @@ const todayMonth = new Date().getMonth();
 const todayYear = new Date().getFullYear();
 
 const notes = {
-    '2025-09-21': {
-                text: 'Jornada 5<br> Elche C.F. - Real Oviedo<br>18:30',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/Kj439Dfc/Elche-C-F-2023-PNG.png',
-                shieldUrlRight: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png'
-            },
-            '2025-09-25': {
-                text: 'Jornada 6<br> Real Oviedo - F.C. Barcelona<br>21:30',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
-                shieldUrlRight: 'https://i.postimg.cc/0yH0rj5X/F-C-Barcelona-C-2007.jpg'
-            },
-            '2025-09-29': {
-                text: 'Jornada 7<br> Valencia C.F. - Real Oviedo<br>21:00',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/pTD6xZ98/Valencia-C-F.png',
-                shieldUrlRight: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png'
-            },
-            '2025-10-04': {
-                text: 'Jornada 8<br> Real Oviedo - Levante U.D.<br>14:00',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
-                shieldUrlRight: 'https://i.postimg.cc/W1FCSyhH/Levante-U-D-PNG.png'
-            },
-            '2025-10-17': {
-                text: 'Jornada 9<br> Real Oviedo - R.C.D. Espanyol<br>21:00',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
-                shieldUrlRight: 'https://i.postimg.cc/ydBThzdg/RCD-Espanyol-2023.png'
-            },
-            '2025-10-26': {
-                text: 'Jornada 10<br> Girona F.C. - Real Oviedo<br>--:--',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/d0xBnVnf/Girona-F-C-2025.png',
-                shieldUrlRight: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png'
-            },
-            '2025-11-02': {
-                text: 'Jornada 11<br> Real Oviedo - C.A. Osasuna<br>--:--',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
-                shieldUrlRight: 'https://i.postimg.cc/mkhYyGr2/C-A-Osasuna-2015.jpg'
-            },
-            '2025-11-09': {
-                text: 'Jornada 12<br> Athletic Club - Real Oviedo<br>--:--',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/wvk3Y2Hv/Athletic-Club-2025.png',
-                shieldUrlRight: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png'
-            },
-            '2025-11-23': {
-                text: 'Jornada 13<br> Real Oviedo - Rayo Vallecano<br>--:--',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
-                shieldUrlRight: 'https://i.postimg.cc/153WzLct/Rayo-Vallecano-de-Madrid-B-2007.jpg'
-            },
-            '2025-11-30': {
-                text: 'Jornada 14<br> Atlético de Madrid - Real Oviedo<br>--:--',
+    
+            
+            
+            '2025-11-29': {
+                text: 'Jornada 14<br> Atlético de Madrid - Real Oviedo<br>21:00',
                 color: 'blue',
                 shieldUrlLeft: 'https://i.postimg.cc/MT6NxX9P/Club-Atl-tico-de-Madrid-2000.jpg',
                 shieldUrlRight: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png'
             },
-            '2025-12-07': {
-                text: 'Jornada 15<br> Real Oviedo - R.C.D. Mallorca<br>--:--',
+            '2025-12-05': {
+                text: 'Jornada 15<br> Real Oviedo - R.C.D. Mallorca<br>21:00',
                 color: 'blue',
                 shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
                 shieldUrlRight: 'https://i.postimg.cc/PryB3WKL/R-C-D-Mallorca-1996.jpg'
             },
             '2025-12-14': {
-                text: 'Jornada 16<br> Sevilla F.C. - Real Oviedo<br>--:--',
+                text: 'Jornada 16<br> Sevilla F.C. - Real Oviedo<br>14:00',
                 color: 'blue',
                 shieldUrlLeft: 'https://i.postimg.cc/KzFSjx5V/Sevilla-F-C-2020.jpg',
                 shieldUrlRight: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png'
             },
-            '2025-12-21': {
-                text: 'Jornada 17<br> Real Oviedo - Celta de Vigo<br>--:--',
+            '2025-12-20': {
+                text: 'Jornada 17<br> Real Oviedo - Celta de Vigo<br>14:00',
                 color: 'blue',
                 shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
                 shieldUrlRight: 'https://i.postimg.cc/GtF1h6kz/R-C-Celta-de-Vigo-2016.jpg'
@@ -213,7 +162,7 @@ const notes = {
             }
 };
 
-const blinkingDays = ['2025-09-21', '2025-09-25', '2025-09-29', '2025-10-04', '2025-10-17', '2025-10-26', '2025-11-02', '2025-11-09', '2025-11-23', '2025-11-30', '2025-12-07', '2025-12-14', '2025-12-21', '2026-01-04', '2026-01-11', '2026-01-18', '2026-01-25', '2026-02-01', '2026-02-08', '2026-02-15', '2026-02-22', '2026-03-01', '2026-03-08', '2026-03-15', '2026-03-22', '2026-04-05', '2026-04-12', '2026-04-19', '2026-04-22', '2026-05-03', '2026-05-10', '2026-05-13', '2026-05-17', '2026-05-24'];
+const blinkingDays = ['2025-11-29', '2025-12-05', '2025-12-14', '2025-12-20', '2026-01-04', '2026-01-11', '2026-01-18', '2026-01-25', '2026-02-01', '2026-02-08', '2026-02-15', '2026-02-22', '2026-03-01', '2026-03-08', '2026-03-15', '2026-03-22', '2026-04-05', '2026-04-12', '2026-04-19', '2026-04-22', '2026-05-03', '2026-05-10', '2026-05-13', '2026-05-17', '2026-05-24'];
 
 function renderCalendar() {
     const monthName = document.getElementById('monthName');
