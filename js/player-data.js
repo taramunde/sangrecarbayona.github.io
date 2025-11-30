a6c83ba59244e0b61afa9532abf37acb85863e0b
taramunde <63210114+taramunde@users.noreply.github.com>
2025-11-30 11:35:08 +0100

    Update player-data.js

--- js/player-data.js
+++ js/player-data.js
@@ -204,8 +204,8 @@ const playersData = {
         ],
         stats: {
             "2025/26": {
-                liga: { partidos: 2, goles_marcados: 0, tarjetas_amarillas: 1, tarjetas_rojas: 0 },
-                copa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
+                liga: { partidos: 3, goles_marcados: 0, tarjetas_amarillas: 1, tarjetas_rojas: 0 },
+                copa: { partidos: 1, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 uefa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 amistosos: { partidos: 0, goles_encajados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
             }
