7bdd66c33d187550e02f1d35361b6cdc9c9682d6
taramunde <63210114+taramunde@users.noreply.github.com>
2025-12-06 16:57:15 +0100

    Update news-data.js

--- js/news-data.js
+++ js/news-data.js
@@ -4,7 +4,7 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/15Qs5NRP/logo_-_La_Voz_de_Asturias.webp",
         sourceName: "La Voz de Asturias",
-        image: "https://i.postimg.cc/wTS4mFvk/J-14-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
+        image: "https://i.postimg.cc/907sx7R5/J_15_lva.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
         title: "El Real Oviedo ya es el segundo equipo menos goleador del siglo XXI en Primera División.",
         author: "PABLO FERNÁNDEZ",
         link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2025/12/06/real-oviedo-segundo-equipo-goleador-siglo-xxi-primera-division/00031765021617593811906.htm"
@@ -12,7 +12,7 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/nV7nJ1J0/logo_-_La_Nueva_España.webp",
         sourceName: "La Nueva España",
-        image: "https://i.postimg.cc/8PqX6w54/J-14-Atleti-Oviedo-LNE.webp", // FOTO NOTICIA 2
+        image: "https://i.postimg.cc/D0XRCXJ2/j_15_lne.webp", // FOTO NOTICIA 2
         title: "El Oviedo sigue seco: sin gol, solo puede empatar con el Mallorca (0-0) en un choque en el que acabó con nueve.",
         author: "NACHO AZPARREN",
         link: "https://www.lne.es/real-oviedo/2025/12/05/ovi-124505110.html"
@@ -20,7 +20,7 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/FRkN1yJF/logo_-_El_Comercio.webp",
         sourceName: "El Comercio",
-        image: "https://i.postimg.cc/RVjpHGFX/J-14-EC.webp", // FOTO NOTICIA 3
+        image: "https://i.postimg.cc/Gty6My8n/j_15_ec.webp", // FOTO NOTICIA 3
         title: "El Real Oviedo abdica pronto en el Metropolitano",
         author: "HUGO VELASCO",
         link: "https://www.elcomercio.es/real-oviedo/atletico-madrid-real-oviedo-directo-partido-futbol-primera-goles-jugadas-20251129170543-di.html"
@@ -28,7 +28,7 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/7hvDz5g3/logo_-_Killer_Asturias.webp",
         sourceName: "Killer Asturias",
-        image: "https://i.postimg.cc/ZK1sdjRj/J-14-KA.jpg", // FOTO NOTICIA 4
+        image: "https://i.postimg.cc/NG2hGWN5/j15_ka.webp", // FOTO NOTICIA 4
         title: "FINAL: Así fue la derrota del Oviedo contra el Atlético de Madrid (2-0)",
         author: "TATO FELGUEROSO",
         link: "https://killerasturias.com/node/16409"
