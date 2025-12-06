02fe94e26012c8565018822008102779f2983c7a
taramunde <63210114+taramunde@users.noreply.github.com>
2025-12-06 10:19:51 +0100

    Update future date for next match

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,6 @@
 // js/timer.js
 function updateTimer() {
-    const future = Date.parse("Dec 05, 2025 21:00:00"); // Fecha del próximo partido (Atlético de Madrid vs Real Oviedo)
+    const future = Date.parse("Dec 14, 2025 14:00:00"); // Fecha del próximo partido (Atlético de Madrid vs Real Oviedo)
     const now = new Date();
     const diff = future - now;
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
