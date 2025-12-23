9c9edaeee154619b5c6b43f4bec3823e8912c614
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2025-12-23 16:56:12 +0100

    Actualización automática

--- js/news-data.js
+++ js/news-data.js
@@ -4,34 +4,34 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/15Qs5NRP/logo_-_La_Voz_de_Asturias.webp",
         sourceName: "La Voz de Asturias",
-        image: "https://i.postimg.cc/907sx7R5/J_15_lva.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
-        title: "El Real Oviedo ya es el segundo equipo menos goleador del siglo XXI en Primera División.",
+        image: "https://i.postimg.cc/YCLwsNHk/J_17_oviedo_celta_lva.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
+        title: "Al Real Oviedo se le olvidó ganar.",
         author: "PABLO FERNÁNDEZ",
-        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2025/12/06/real-oviedo-segundo-equipo-goleador-siglo-xxi-primera-division/00031765021617593811906.htm"
+        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2025/12/20/oviedo-celta/00031766239307317499299.htm"
     },
     {
         sourceLogo: "https://i.postimg.cc/nV7nJ1J0/logo_-_La_Nueva_España.webp",
         sourceName: "La Nueva España",
-        image: "https://i.postimg.cc/D0XRCXJ2/j_15_lne.webp", // FOTO NOTICIA 2
-        title: "El Oviedo sigue seco: sin gol, solo puede empatar con el Mallorca (0-0) en un choque en el que acabó con nueve.",
+        image: "https://i.postimg.cc/RFyYfyWc/J_17_Oviedo_Celta_LNE.jpg", // FOTO NOTICIA 2
+        title: "Más de lo mismo: un Oviedo sin gol firma otro empate insuficiente ante el Celta (0-0).",
         author: "NACHO AZPARREN",
-        link: "https://www.lne.es/real-oviedo/2025/12/05/ovi-124505110.html"
+        link: "https://www.lne.es/real-oviedo/2025/12/20/oviedo-gol-firma-empate-insuficiente-124994492.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/FRkN1yJF/logo_-_El_Comercio.webp",
         sourceName: "El Comercio",
-        image: "https://i.postimg.cc/Gty6My8n/j_15_ec.webp", // FOTO NOTICIA 3
-        title: "Real Oviedo 0-0 Mallorca: La falta de gol condena una vez más al Oviedo.",
+        image: "https://i.postimg.cc/D0C8BQK4/J_17_Oviedo_Celta_EC.webp", // FOTO NOTICIA 3
+        title: "La mejoría del Real Oviedo de Almada no basta para ganar al Celta.",
         author: "RAMÓN JULIO",
-        link: "https://www.elcomercio.es/real-oviedo/real-oviedo-mallorca-falta-gol-condena-vez-20251205233147-nt.html"
+        link: "https://www.elcomercio.es/real-oviedo/directo-real-oviedo-celta-vigo-liga-primera-20251220111100-di.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/7hvDz5g3/logo_-_Killer_Asturias.webp",
         sourceName: "Killer Asturias",
-        image: "https://i.postimg.cc/NG2hGWN5/j15_ka.webp", // FOTO NOTICIA 4
-        title: "La Crónica: El mejor Oviedo acaba desquiciado ante el Mallorca.",
+        image: "https://i.postimg.cc/PxXX9LyS/J_17_Oviedo_Celta_KA.jpg", // FOTO NOTICIA 4
+        title: "La Crónica: El Oviedo sigue incapaz de hacer gol ante un tímido Celta.",
         author: "JUAN MORENO",
-        link: "https://killerasturias.com/cronicas/la-cronica-el-mejor-oviedo-acaba-desquiciado-ante-el-mallorca"
+        link: "https://killerasturias.com/cronicas/la-cronica-el-oviedo-sigue-incapaz-de-hacer-gol-ante-un-timido-celta"
     }
 ];
 
