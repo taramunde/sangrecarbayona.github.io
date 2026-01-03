aa7a30e22003dc587323fcec08ec50895b79c1ff
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-01-04 00:59:39 +0100

    Actualización automática

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -233,11 +233,11 @@ const enfrentamientos = [
   
   // Jornada 18
   { equipo1: "Deportivo Alavés", equipo2: "Real Oviedo", goles1: null, goles2: null },
-  { equipo1: "Celta de Vigo", equipo2: "Valencia C.F.", goles1: null, goles2: null },
-  { equipo1: "Elche C.F.", equipo2: "Villarreal C.F.", goles1: null, goles2: null },
-  { equipo1: "C.A. Osasuna", equipo2: "Athletic Club", goles1: null, goles2: null },
+  { equipo1: "Celta de Vigo", equipo2: "Valencia C.F.", goles1: 4, goles2: 1 },
+  { equipo1: "Elche C.F.", equipo2: "Villarreal C.F.", goles1: 1, goles2: 3 },
+  { equipo1: "C.A. Osasuna", equipo2: "Athletic Club", goles1: 1, goles2: 1 },
   { equipo1: "Real Sociedad", equipo2: "Atlético de Madrid", goles1: null, goles2: null },
-  { equipo1: "R.C.D. Espanyol", equipo2: "F.C. Barcelona", goles1: null, goles2: null },
+  { equipo1: "R.C.D. Espanyol", equipo2: "F.C. Barcelona", goles1: 0, goles2: 2 },
   { equipo1: "Real Madrid", equipo2: "Real Betis", goles1: null, goles2: null },
   { equipo1: "Rayo Vallecano", equipo2: "Getafe C.F.", goles1: 1, goles2: 1 },
   { equipo1: "R.C.D. Mallorca", equipo2: "Girona F.C.", goles1: null, goles2: null },
