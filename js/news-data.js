fd4bb4bb790e88947470abf05baf5071bdbfebb1
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-01-10 16:34:10 +0100

    Actualización automática

--- js/news-data.js
+++ js/news-data.js
@@ -4,34 +4,34 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/15Qs5NRP/logo_-_La_Voz_de_Asturias.webp",
         sourceName: "La Voz de Asturias",
-        image: "https://i.postimg.cc/CKR0DhpT/j18-lva.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
-        title: "No hay manera con este Real Oviedo.",
+        image: "https://i.postimg.cc/6Qn24b1j/J-19-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
+        title: "Otro punto que no saca de pobre al Real Oviedo.",
         author: "PABLO FERNÁNDEZ",
-        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/01/04/cronica/00031767551865008203965.htm"
+        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/01/10/oviedo-betis/00031768053623707107721.htm"
     },
     {
         sourceLogo: "https://i.postimg.cc/nV7nJ1J0/logo_-_La_Nueva_España.webp",
         sourceName: "La Nueva España",
-        image: "https://i.postimg.cc/L8TBcPXk/j18-lne.jpg", // FOTO NOTICIA 2
-        title: "Un Oviedo con brotes verdes se sacude el gafe del gol, pero falla mucho y lo paga caro en Mendizorroza (1-1)",
-        author: "JOAQUÍN ALONSO",
-        link: "https://www.lne.es/real-oviedo/2026/01/04/oviedo-sacude-gafe-gol-falla-125370770.html"
+        image: "https://i.postimg.cc/tgWZnvmv/J-19-LNE.jpg", // FOTO NOTICIA 2
+        title: "Un final amargo evita la victoria de un valiente Oviedo ante el Betis",
+        author: "NACHO AZPARREN",
+        link: "https://www.lne.es/real-oviedo/2026/01/10/final-amargo-evita-victoria-valiente-125554499.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/FRkN1yJF/logo_-_El_Comercio.webp",
         sourceName: "El Comercio",
-        image: "https://i.postimg.cc/yd6Fnbjw/j18-ec.webp", // FOTO NOTICIA 3
-        title: "El mejor Oviedo del curso vuelve a perdonar.",
-        author: "RAMÓN JULIO",
-        link: "https://www.elcomercio.es/real-oviedo/mejor-oviedo-curso-vuelve-perdonar-20260105213702-nt.html"
+        image: "https://i.postimg.cc/g08XLtB7/J-19-EC.webp", // FOTO NOTICIA 3
+        title: "Real Oviedo 1-1 Betis: Reparto de puntos en el Tartiere.",
+        author: "HUGO VELASCO",
+        link: "https://www.elcomercio.es/real-oviedo/real-oviedo-betis-directo-partido-futbol-goles-jugadas-primera-20260110132809-di.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/7hvDz5g3/logo_-_Killer_Asturias.webp",
         sourceName: "Killer Asturias",
-        image: "https://i.postimg.cc/CKR0DhpT/j18-lva.webp", // FOTO NOTICIA 4
-        title: "La Crónica: Un gran Oviedo se deja dos puntos en Mendizorroza.",
-        author: "JUAN MORENO",
-        link: "https://killerasturias.com/cronicas/la-cronica-un-gran-oviedo-se-deja-dos-puntos-en-mendizorroza"
+        image: "https://i.postimg.cc/JhZDHYdd/J-19-KA.png", // FOTO NOTICIA 4
+        title: "FINAL: Así fue el empate entre Real Oviedo y Real Betis (1-1).",
+        author: "TATO FELGUEROSO",
+        link: "https://killerasturias.com/noticias/en-directo-real-oviedo-0-0-real-betis"
     }
 ];
 
