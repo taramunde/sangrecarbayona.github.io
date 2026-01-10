5796e8f4c4b5872d9769bd449754a6dbbb9e63fd
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-01-10 19:27:33 +0100

    Actualización automática

--- js/calendar.js
+++ js/calendar.js
@@ -10,12 +10,7 @@ const notes = {
     
             
             
-            '2026-01-10': {
-                text: 'Jornada 19<br> Real Oviedo - Real Betis<br>14:00',
-                color: 'blue',
-                shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
-                shieldUrlRight: 'https://i.postimg.cc/J49HDXVF/Real-Betis-Balompi-2016.jpg'
-            },
+            
             '2026-01-17': {
                 text: 'Jornada 20<br> C.A. Osasuna - Real Oviedo<br>18:30',
                 color: 'blue',
@@ -23,13 +18,13 @@ const notes = {
                 shieldUrlRight: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png'
             },
             '2026-01-25': {
-                text: 'Jornada 21<br> F.C. Barcelona - Real Oviedo<br>--:--',
+                text: 'Jornada 21<br> F.C. Barcelona - Real Oviedo<br>16:15',
                 color: 'blue',
                 shieldUrlLeft: 'https://i.postimg.cc/0yH0rj5X/F-C-Barcelona-C-2007.jpg',
                 shieldUrlRight: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png'
             },
-            '2026-02-01': {
-                text: 'Jornada 22<br> Real Oviedo - Girona F.C.<br>--:--',
+            '2026-01-31': {
+                text: 'Jornada 22<br> Real Oviedo - Girona F.C.<br>14:00',
                 color: 'blue',
                 shieldUrlLeft: 'https://i.postimg.cc/2yPVD3F0/Real-Oviedo-2019-actualidad.png',
                 shieldUrlRight: 'https://i.postimg.cc/d0xBnVnf/Girona-F-C-2025.png'
@@ -132,7 +127,7 @@ const notes = {
             }
 };
 
-const blinkingDays = ['2026-01-10', '2026-01-18', '2026-01-25', '2026-02-01', '2026-02-08', '2026-02-15', '2026-02-22', '2026-03-01', '2026-03-08', '2026-03-15', '2026-03-22', '2026-04-05', '2026-04-12', '2026-04-19', '2026-04-22', '2026-05-03', '2026-05-10', '2026-05-13', '2026-05-17', '2026-05-24'];
+const blinkingDays = ['2026-01-17', '2026-01-25', '2026-01-31', '2026-02-08', '2026-02-15', '2026-02-22', '2026-03-01', '2026-03-08', '2026-03-15', '2026-03-22', '2026-04-05', '2026-04-12', '2026-04-19', '2026-04-22', '2026-05-03', '2026-05-10', '2026-05-13', '2026-05-17', '2026-05-24'];
 
 function renderCalendar() {
     const monthName = document.getElementById('monthName');
