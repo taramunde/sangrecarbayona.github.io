35bb9edcfa2240845ce1e07fd8d925bd201bb1ee
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-01-10 16:02:08 +0100

    Actualización automática

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -245,11 +245,11 @@ const enfrentamientos = [
   
   // Jornada 19
   { equipo1: "Athletic Club", equipo2: "Real Madrid", goles1: 0, goles2: 3 },
-  { equipo1: "Getafe C.F.", equipo2: "Real Sociedad", goles1: null, goles2: null },
+  { equipo1: "Getafe C.F.", equipo2: "Real Sociedad", goles1: 1, goles2: 2 },
   { equipo1: "Girona F.C.", equipo2: "C.A. Osasuna", goles1: null, goles2: null },
   { equipo1: "Villarreal C.F.", equipo2: "Deportivo Alavés", goles1: null, goles2: null },
   { equipo1: "F.C. Barcelona", equipo2: "Atlético de Madrid", goles1: 3, goles2: 1 },
-  { equipo1: "Real Oviedo", equipo2: "Real Betis", goles1: null, goles2: null },
+  { equipo1: "Real Oviedo", equipo2: "Real Betis", goles1: 1, goles2: 1 },
   { equipo1: "Sevilla F.C.", equipo2: "Celta de Vigo", goles1: null, goles2: null },
   { equipo1: "Valencia C.F.", equipo2: "Elche C.F.", goles1: null, goles2: null },
   { equipo1: "Levante U.D.", equipo2: "R.C.D. Espanyol", goles1: null, goles2: null },
--- js/player-data.js
+++ js/player-data.js
@@ -37,7 +37,7 @@ const playersData = {
         ],
         stats: {
             "2025/26": {
-                liga: { partidos: 19, goles_encajados: 27, tarjetas_amarillas: 1, tarjetas_rojas: 0 },
+                liga: { partidos: 19, goles_encajados: 28, tarjetas_amarillas: 1, tarjetas_rojas: 0 },
                 copa: { partidos: 0, goles_encajados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 uefa: { partidos: 0, goles_encajados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 amistosos: { partidos: 4, goles_encajados: 3, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
@@ -1147,7 +1147,7 @@ const playersData = {
         
         stats: {
             "2025/26": {
-                liga: { partidos: 7, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
+                liga: { partidos: 8, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 copa: { partidos: 1, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 uefa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 amistosos: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
@@ -2186,7 +2186,7 @@ const playersData = {
         
         stats: {
             "2025/26": {
-                liga: { partidos: 2, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
+                liga: { partidos: 2, goles_marcados: 0, tarjetas_amarillas: 1, tarjetas_rojas: 0 },
                 copa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 uefa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 amistosos: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
@@ -2821,7 +2821,7 @@ const playersData = {
 
         stats: {
             "2025/26": {
-                liga: { partidos: 15, goles_marcados: 1, tarjetas_amarillas: 2, tarjetas_rojas: 0 },
+                liga: { partidos: 16, goles_marcados: 1, tarjetas_amarillas: 2, tarjetas_rojas: 0 },
                 copa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 uefa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 amistosos: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
@@ -4893,7 +4893,7 @@ const playersData = {
          
          stats: {
             "2025/26": {
-                liga: { partidos: 1, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
+                liga: { partidos: 1, goles_marcados: 0, tarjetas_amarillas: 1, tarjetas_rojas: 0 },
                 copa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 uefa: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 },
                 amistosos: { partidos: 0, goles_marcados: 0, tarjetas_amarillas: 0, tarjetas_rojas: 0 }
