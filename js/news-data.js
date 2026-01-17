74172ae38d78012abadfa0a1d0353e57ac67cc85
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-01-17 21:05:42 +0100

    Actualización automática

--- js/news-data.js
+++ js/news-data.js
@@ -4,34 +4,34 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/15Qs5NRP/logo_-_La_Voz_de_Asturias.webp",
         sourceName: "La Voz de Asturias",
-        image: "https://i.postimg.cc/6Qn24b1j/J-19-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
-        title: "Otro punto que no saca de pobre al Real Oviedo.",
+        image: "https://i.postimg.cc/htpV3LVW/J-20-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
+        title: "Osasuna y Quintero González hunden al Real Oviedo.",
         author: "PABLO FERNÁNDEZ",
-        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/01/10/oviedo-betis/00031768053623707107721.htm"
+        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/01/17/osa-ovi/00031768674841391908953.htm"
     },
     {
         sourceLogo: "https://i.postimg.cc/nV7nJ1J0/logo_-_La_Nueva_España.webp",
         sourceName: "La Nueva España",
-        image: "https://i.postimg.cc/tgWZnvmv/J-19-LNE.jpg", // FOTO NOTICIA 2
-        title: "Un final amargo evita la victoria de un valiente Oviedo ante el Betis",
+        image: "https://i.postimg.cc/YSFqmkrC/J20-LNE.jpg", // FOTO NOTICIA 2
+        title: "Al Oviedo le hunden sus propios errores: derrota 3-2 en Pamplona.",
         author: "NACHO AZPARREN",
-        link: "https://www.lne.es/real-oviedo/2026/01/10/final-amargo-evita-victoria-valiente-125554499.html"
+        link: "https://www.lne.es/real-oviedo/2026/01/17/oviedo-le-hunden-propios-errores-125793650.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/FRkN1yJF/logo_-_El_Comercio.webp",
         sourceName: "El Comercio",
-        image: "https://i.postimg.cc/g08XLtB7/J-19-EC.webp", // FOTO NOTICIA 3
-        title: "Real Oviedo 1-1 Betis: Reparto de puntos en el Tartiere.",
+        image: "https://i.postimg.cc/XJVS70pN/J20-EC.webp", // FOTO NOTICIA 3
+        title: "Osasuna 3-2 Real Oviedo | El fútbol da la espalda al Real Oviedo.",
         author: "HUGO VELASCO",
-        link: "https://www.elcomercio.es/real-oviedo/real-oviedo-betis-directo-partido-futbol-goles-jugadas-primera-20260110132809-di.html"
+        link: "https://www.elcomercio.es/real-oviedo/directo-osasuna-real-oviedo-partido-futbol-primera-division-20260117180016-di.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/7hvDz5g3/logo_-_Killer_Asturias.webp",
         sourceName: "Killer Asturias",
-        image: "https://i.postimg.cc/JhZDHYdd/J-19-KA.png", // FOTO NOTICIA 4
-        title: "FINAL: Así fue el empate entre Real Oviedo y Real Betis (1-1).",
-        author: "TATO FELGUEROSO",
-        link: "https://killerasturias.com/noticias/en-directo-real-oviedo-0-0-real-betis"
+        image: "https://i.postimg.cc/x8h4HrT5/J20-KA.jpg", // FOTO NOTICIA 4
+        title: "La Crónica: El Var deja al Oviedo en la UCI.",
+        author: "JUAN MORENO",
+        link: "https://killerasturias.com/cronicas/la-cronica-el-var-deja-al-oviedo-en-la-uci"
     }
 ];
 
