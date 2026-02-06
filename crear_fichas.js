9f619b63428788e2b1ee27aefb7961cc0e6ca693
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-02-06 19:19:10 +0100

    Actualización automática

--- crear_fichas.js
+++ crear_fichas.js
@@ -46,7 +46,7 @@ for (let id in jugadores) {
         <div class="ribbon-container"><div class="ribbon ribbon-bottom">${nombre}</div></div>
     </div>
 
-    <svg width="0" height="0" style="position:absolute;">
+    <svg width="0" height="0" style="position:absolute; z-index:-1;">
         <defs>
             <clipPath id="svgPath" clipPathUnits="userSpaceOnUse">
                 <path d="M265.3 53.9a33.3 33.3 0 0 1-17.8-5.5 32 32 0 0 1-13.7-22.9c-.2-1.1-.4-2.3-.4-3.4 0-1.3-1-1.5-1.8-1.9a163 163 0 0 0-31-18.2A164.7 164.7 0 0 0 132.5 0c-23.7 0-46.3.7-68 2a163 163 0 0 0-31 18.2c-.8.4-1.8.6-1.8 1.9 0 1.1-.2 2.3-.4 3.4a32 32 0 0 1-13.7 22.9A33.3 33.3 0 0 1 0 53.9c0 68.4 2.3 124.6 15 166.9 11.5 38.5 40 68.3 80.4 84 10.4 4 23 5.2 37.1 5.2s26.7-1.2 37.1-5.2c40.4-15.7 68.9-45.5 80.4-84 12.7-42.3 15-98.5 15-166.9z"/>
