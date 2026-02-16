a6252bd6dd24e1b9dac711c8b365bdef3291c264
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-02-16 01:24:48 +0100

    Actualización automática

--- js/videos-data.js
+++ js/videos-data.js
@@ -1,23 +1,110 @@
 const videosDB = [
     {
         id: "6t-g2jTvmoo", // ID del vídeo de Youtube
-        title: "Villarreal C.F. Vs Real Oviedo"
+        title: "J1 Villarreal C.F. Vs Real Oviedo"
     },
     {
-        id: "X8K4vQ9fG_s",
-        title: "Goles: Cazorla vuelve a brillar en el Tartiere"
+        id: "7J0I0utH3nA",
+        title: "J2 Real Oviedo Vs Real Madrid"
     },
     {
-        id: "abc12345", // Inventado, sustitúyelo
-        title: "Rueda de prensa post-partido"
+        id: "thWZDrnrewI", 
+        title: "J3 Real Oviedo Vs Real Sociedad"
     },
     {
-        id: "xyz98765", 
-        title: "Entrenamiento a puerta abierta"
+        id: "yglPhrGYXzM", 
+        title: "J4 Getafe C.F. Vs Real Oviedo"
     },
-    // Añade más bloques aquí...
     {
-        id: "ejemploID",
-        title: "Título del vídeo nuevo"
+        id: "0TPftccJqsM",
+        title: "J5 Elche C.F. Vs Real Oviedo"
+    },
+    {
+        id: "fo-FHUoywT8",
+        title: "J6 Real Oviedo Vs F.C. Barcelona"
+    },
+    {
+        id: "bI_704mWyu0",
+        title: "J7 Valencia C.F. Vs Real Oviedo"
+    },
+    {
+        id: "UaSjS9ywDwp72XYW",
+        title: "J8 Real Oviedo Vs Levante U.D."
+    },
+    {
+        id: "tYdH3FakT5s",
+        title: "J9 Real Oviedo Vs R.C.D. Espanyol"
+    },
+    {
+        id: "hcsHWDUPr2rhhl1P",
+        title: "J10 Girona F.C. Vs Real Oviedo"
+    },
+    {
+        id: "51BWyMdJAAxQ9jz0",
+        title: "C. del Rey - 1ª Ronda Ourense C.F. Vs Real Oviedo"
+    },
+    {
+        id: "cZPCCnuYZ1BcPYso",
+        title: "J11 Real Oviedo Vs C.A. Osasuna"
+    },
+    {
+        id: "B7l_BiWLVcA",
+        title: "J12 Athletic Club Vs Real Oviedo"
+    },
+    {
+        id: "7g4nZMjdvAk",
+        title: "J13 Real Oviedo Vs Rayo Vallecano"
+    },
+    {
+        id: "9VxYdm7Y17Y",
+        title: "J14 At. Madrid Vs Real Oviedo"
+    },
+    {
+        id: "DD5Rn_vkrxWPFU4C",
+        title: "J15 Real Oviedo Vs R.C.D. Mallorca"
+    },
+    {
+        id: "YycQM9touZU",
+        title: "J16 Sevilla F.C. Vs Real Oviedo"
+    },
+    {
+        id: "yQ5k9Or2wGEBJZQd",
+        title: "J17 Real Oviedo Vs Celta de Vigo"
+    },
+    {
+        id: "AULDRmmpANGJ0-Y_",
+        title: "J18 Deportivo Alavés Vs Real Oviedo"
+    },
+    {
+        id: "N9b3ZWva7qM",
+        title: "J19 Real Oviedo Vs Real Betis"
+    },
+    {
+        id: "4-m5NXluPwE",
+        title: "J20 C.A. Osasuna Vs Real Oviedo"
+    },
+    {
+        id: "qzvQXVHkW_U5_qMC",
+        title: "J21 F.C. Barcelona Vs Real Oviedo"
+    },
+    {
+        id: "qzvQXVHkW_U5_qMC",
+        title: "J22 Real Oviedo Vs Girona F.C."
+    },
+    {
+        id: "C5E4g_uy9PE",
+        title: "J23 Rayo Vallecano Vs Real Oviedo"
+    },
+    {
+        id: "35g46UDxkTk",
+        title: "J24 Real Oviedo Vs Athletic Club"
     }
+
+
+
+
+
+
+
+
 ];
