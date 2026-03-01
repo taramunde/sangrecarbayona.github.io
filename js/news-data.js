0cf78ff76ebde7585083efed871e96b7d3a0283e
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-03-01 16:20:02 +0100

    Actualización automática

--- js/news-data.js
+++ js/news-data.js
@@ -4,34 +4,34 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/15Qs5NRP/logo_-_La_Voz_de_Asturias.webp",
         sourceName: "La Voz de Asturias",
-        image: "https://i.postimg.cc/rFJbpVkB/J-25-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
-        title: "El Real Oviedo desperdicia otro 0-2 y empata en el descuento en un partido loco.",
+        image: "https://i.postimg.cc/cCxpPL7G/J-26-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
+        title: "Castigo demasiado cruel para un notable Real Oviedo: 0-1 de Julián Álvarez en el descuento.",
         author: "PABLO FERNÁNDEZ",
-        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/02/21/real-oviedo-desperdicia-0-2-empata-descuento-partido-loco/00031771682674685983903.htm"
+        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/02/28/dfg/00031772312324904653353.htm"
     },
     {
         sourceLogo: "https://i.postimg.cc/nV7nJ1J0/logo_-_La_Nueva_España.webp",
         sourceName: "La Nueva España",
-        image: "https://i.postimg.cc/Vkv7LX4k/J-25-LNE.jpg", // FOTO NOTICIA 2
-        title: "Y otra vez, otra remontada en contra: el Oviedo firma las tablas en Anoeta tras ir con una ventaja de dos goles (3-3).",
-        author: "JOAQUÍN ALONSO",
-        link: "https://www.lne.es/real-oviedo/2026/02/21/vez-remontada-oviedo-firma-tablas-127109282.html"
+        image: "https://i.postimg.cc/QCcqgQKM/J-26-LNE.jpg", // FOTO NOTICIA 2
+        title: "Julián congela la esperanza: cruel derrota de un buen Oviedo en el minuto 94 (0-1).",
+        author: "NACHO AZPARREN",
+        link: "https://www.lne.es/real-oviedo/2026/02/28/julian-congela-esperanza-cruel-derrota-127399369.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/FRkN1yJF/logo_-_El_Comercio.webp",
         sourceName: "El Comercio",
-        image: "https://i.postimg.cc/m2XwqtRW/J-25-EC.webp", // FOTO NOTICIA 3
-        title: "Real Sociedad 3-3 Real Oviedo: Los azules desaprovechan una ventaja de dos goles en la segunda parte.",
-        author: "HUGO VELASCO",
-        link: "https://www.elcomercio.es/real-oviedo/directo-real-sociedad-real-oviedo-partido-primera-division-laliga-20260221102148-di.html"
+        image: "https://i.postimg.cc/mrmsyMWs/J-26-EC.webp", // FOTO NOTICIA 3
+        title: "Un Real Oviedo notable cae con crueldad e injusticia.",
+        author: "RAMÓN JULIO",
+        link: "https://www.elcomercio.es/real-oviedo/real-oviedo-atletico-madrid-notable-cae-crueldad-injusticia-20260301234213-nt.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/7hvDz5g3/logo_-_Killer_Asturias.webp",
         sourceName: "Killer Asturias",
-        image: "https://i.postimg.cc/xTCGKnmt/J-25-KO.jpg", // FOTO NOTICIA 4
-        title: "La Crónica: El Oviedo revive el síndrome de Girona en Anoeta.",
+        image: "https://i.postimg.cc/Z57mrphf/J-26-KA.jpg", // FOTO NOTICIA 4
+        title: "La Crónica: Julián Álvarez da la puntilla al Oviedo.",
         author: "JUAN MORENO",
-        link: "https://killerasturias.com/cronicas/la-cronica-el-oviedo-revive-el-sindrome-de-girona-en-anoeta"
+        link: "https://killerasturias.com/cronicas/la-cronica-julian-alvarez-da-la-puntilla-al-oviedo"
     }
 ];
 
