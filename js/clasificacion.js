fb4eb722cd97d542ce04391ebeb6839ec26b4153
Taramunde Sangre Carbayona <taramunde1974@gmail.com>
2026-03-14 23:35:31 +0100

    Actualización automática

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -358,7 +358,7 @@ const enfrentamientos = [
   { equipo1: "Real Betis", equipo2: "Celta de Vigo", goles1: null, goles2: null },
   { equipo1: "Real Oviedo", equipo2: "Valencia C.F.", goles1: 1, goles2: 0 },
   { equipo1: "Girona F.C.", equipo2: "Athletic Club", goles1: 3, goles2: 0 },
-  { equipo1: "Real Madrid", equipo2: "Elche C.F.", goles1: null, goles2: null },
+  { equipo1: "Real Madrid", equipo2: "Elche C.F.", goles1: 4, goles2: 1 },
   { equipo1: "R.C.D. Mallorca", equipo2: "R.C.D. Espanyol", goles1: null, goles2: null },
   { equipo1: "Rayo Vallecano", equipo2: "Levante U.D.", goles1: null, goles2: null },
   { equipo1: "Real Sociedad", equipo2: "C.A. Osasuna", goles1: null, goles2: null },
