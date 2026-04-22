560381a32bf9e7da5fa532d26e2b7d23dbfb01e7
taramunde <taramunde1974@gmail.com>
2026-04-22 15:56:18 +0200

    v

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -412,14 +412,14 @@ const enfrentamientos = [
   { equipo1: "Valencia C.F.", equipo2: "Girona F.C.", goles1: null, goles2: null },
   
   // Jornada 33
-  { equipo1: "Athletic Club", equipo2: "C.A. Osasuna", goles1: null, goles2: null },
+  { equipo1: "Athletic Club", equipo2: "C.A. Osasuna", goles1: 1, goles2: 0 },
   { equipo1: "F.C. Barcelona", equipo2: "Celta de Vigo", goles1: null, goles2: null },
   { equipo1: "Levante U.D.", equipo2: "Sevilla F.C.", goles1: null, goles2: null },
-  { equipo1: "R.C.D. Mallorca", equipo2: "Valencia C.F.", goles1: null, goles2: null },
+  { equipo1: "R.C.D. Mallorca", equipo2: "Valencia C.F.", goles1: 1, goles2: 1 },
   { equipo1: "Real Oviedo", equipo2: "Villarreal C.F.", goles1: null, goles2: null },
-  { equipo1: "Real Madrid", equipo2: "Deportivo Alavés", goles1: null, goles2: null },
+  { equipo1: "Real Madrid", equipo2: "Deportivo Alavés", goles1: 2, goles2: 1 },
   { equipo1: "Elche C.F.", equipo2: "Atlético de Madrid", goles1: null, goles2: null },
-  { equipo1: "Girona F.C.", equipo2: "Real Betis", goles1: null, goles2: null },
+  { equipo1: "Girona F.C.", equipo2: "Real Betis", goles1: 2, goles2: 3 },
   { equipo1: "Rayo Vallecano", equipo2: "R.C.D. Espanyol", goles1: null, goles2: null },
   { equipo1: "Real Sociedad", equipo2: "Getafe C.F.", goles1: null, goles2: null },
   
