40e4a90072e3f6ffd532dfee7fdbbc8808a87640
taramunde <taramunde1974@gmail.com>
2026-05-04 10:24:38 +0200

    gg

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -425,10 +425,10 @@ const enfrentamientos = [
   
   // Jornada 34
   { equipo1: "Deportivo Alavés", equipo2: "Athletic Club", goles1: 2, goles2: 4 },
-  { equipo1: "Real Betis", equipo2: "Real Oviedo", goles1: null, goles2: null },
+  { equipo1: "Real Betis", equipo2: "Real Oviedo", goles1: 3, goles2: 0 },
   { equipo1: "Celta de Vigo", equipo2: "Elche C.F.", goles1: 3, goles2: 1 },
-  { equipo1: "R.C.D. Espanyol", equipo2: "Real Madrid", goles1: null, goles2: null },
-  { equipo1: "Getafe C.F.", equipo2: "Rayo Vallecano", goles1: null, goles2: null },
+  { equipo1: "R.C.D. Espanyol", equipo2: "Real Madrid", goles1: 0, goles2: 2 },
+  { equipo1: "Getafe C.F.", equipo2: "Rayo Vallecano", goles1: 0, goles2: 2 },
   { equipo1: "Girona F.C.", equipo2: "R.C.D. Mallorca", goles1: 0, goles2: 1 },
   { equipo1: "Valencia C.F.", equipo2: "Atlético de Madrid", goles1: 0, goles2: 2 },
   { equipo1: "C.A. Osasuna", equipo2: "F.C. Barcelona", goles1: 1, goles2: 2 },
--- js/news-data.js
+++ js/news-data.js
@@ -4,34 +4,34 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/15Qs5NRP/logo_-_La_Voz_de_Asturias.webp",
         sourceName: "La Voz de Asturias",
-        image: "https://i.postimg.cc/4NCCz4nP/J-32-LVA.jpg", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
-        title: "El Real Oviedo, tocado y casi hundido.",
+        image: "https://i.postimg.cc/d3B2164W/J-34-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
+        title: "El Betis se aprovecha de los fallos del Real Oviedo.",
         author: "PABLO FERNÁNDEZ",
-        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/04/26/real-oviedo-tocado-hundido/00031777216824682588336.htm"
+        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/05/03/betis-ovi/00031777829093136246760.htm"
     },
     {
         sourceLogo: "https://i.postimg.cc/nV7nJ1J0/logo_-_La_Nueva_España.webp",
         sourceName: "La Nueva España",
-        image: "https://i.postimg.cc/8P88hksb/J-32-LNE.webp", // FOTO NOTICIA 2
-        title: "Al Oviedo se le escapa la vida: derrota con aroma a Segunda ante el Elche (1-2).",
+        image: "https://i.postimg.cc/T19Vw0C4/J-34-LNE.jpg", // FOTO NOTICIA 2
+        title: "El Oviedo elige no pelear: goleada ante un Betis que se aprovechó de los errores azules (3-0).",
         author: "NACHO AZPARREN",
-        link: "https://www.lne.es/real-oviedo/2026/04/26/oviedo-le-escapa-vida-derrota-129546517.html"
+        link: "https://www.lne.es/real-oviedo/2026/05/03/oviedo-elige-pelear-goleada-betis-129803829.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/FRkN1yJF/logo_-_El_Comercio.webp",
         sourceName: "El Comercio",
-        image: "https://i.postimg.cc/MKkkyZXD/J-32-EC.jpg", // FOTO NOTICIA 3
-        title: "Un Real Oviedo negado queda a un paso de Segunda.",
+        image: "https://i.postimg.cc/zvjCB70M/J-34-EC.webp", // FOTO NOTICIA 3
+        title: "El Betis hurga en la herida de un flojo Real Oviedo.",
         author: "RAMÓN JULIO",
-        link: "https://www.elcomercio.es/real-oviedo/real-oviedo-elche-paso-segunda-20260426185151-nt.html"
+        link: "https://www.elcomercio.es/real-oviedo/betis-hurga-herida-flojo-real-oviedo-20260503214712-nt.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/7hvDz5g3/logo_-_Killer_Asturias.webp",
         sourceName: "Killer Asturias",
-        image: "https://i.postimg.cc/ZKkkrY0j/J-32-KA.jpg", // FOTO NOTICIA 4
-        title: "La Crónica: El Elche da la estocada definitiva al Oviedo.",
+        image: "https://i.postimg.cc/QCbQtm0z/J-34-KA.jpg", // FOTO NOTICIA 4
+        title: "La Crónica: El Betis y el VAR castigan a un competitivo Oviedo.",
         author: "JUAN MORENO",
-        link: "https://killerasturias.com/cronicas/la-cronica-el-elche-da-la-estocada-definitiva-al-oviedo"
+        link: "https://killerasturias.com/cronicas/la-cronica-el-betis-y-el-var-castigan-un-competitivo-oviedo"
     }
 ];
 
