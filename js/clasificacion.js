4836bc463727c9cc906eeb04a702ba8627469b9c
taramunde <taramunde1974@gmail.com>
2026-05-10 14:56:07 +0200

    bg

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -437,13 +437,13 @@ const enfrentamientos = [
   
    // Jornada 35
   { equipo1: "Athletic Club", equipo2: "Valencia C.F.", goles1: null, goles2: null },
-  { equipo1: "Atlético de Madrid", equipo2: "Celta de Vigo", goles1: null, goles2: null },
+  { equipo1: "Atlético de Madrid", equipo2: "Celta de Vigo", goles1: 0, goles2: 1 },
   { equipo1: "F.C. Barcelona", equipo2: "Real Madrid", goles1: null, goles2: null },
-  { equipo1: "Levante U.D.", equipo2: "C.A. Osasuna", goles1: null, goles2: null },
+  { equipo1: "Levante U.D.", equipo2: "C.A. Osasuna", goles1: 3, goles2: 2 },
   { equipo1: "R.C.D. Mallorca", equipo2: "Villarreal C.F.", goles1: null, goles2: null },
-  { equipo1: "Elche C.F.", equipo2: "Deportivo Alavés", goles1: null, goles2: null },
-  { equipo1: "Real Sociedad", equipo2: "Real Betis", goles1: null, goles2: null },
-  { equipo1: "Sevilla F.C.", equipo2: "R.C.D. Espanyol", goles1: null, goles2: null },
+  { equipo1: "Elche C.F.", equipo2: "Deportivo Alavés", goles1: 1, goles2: 1 },
+  { equipo1: "Real Sociedad", equipo2: "Real Betis", goles1: 2, goles2: 2 },
+  { equipo1: "Sevilla F.C.", equipo2: "R.C.D. Espanyol", goles1: 2, goles2: 1 },
   { equipo1: "Real Oviedo", equipo2: "Getafe C.F.", goles1: null, goles2: null },
   { equipo1: "Rayo Vallecano", equipo2: "Girona F.C.", goles1: null, goles2: null },
   
