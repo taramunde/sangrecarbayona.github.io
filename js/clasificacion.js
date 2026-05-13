cadbc6590ffee6ab46324f994faaca4ed2e001ca
taramunde <taramunde1974@gmail.com>
2026-05-13 19:38:28 +0200

    f

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -449,12 +449,12 @@ const enfrentamientos = [
   
   // Jornada 36
   { equipo1: "Deportivo Alavés", equipo2: "F.C. Barcelona", goles1: null, goles2: null },
-  { equipo1: "Real Betis", equipo2: "Elche C.F.", goles1: null, goles2: null },
-  { equipo1: "Celta de Vigo", equipo2: "Levante U.D.", goles1: null, goles2: null },
+  { equipo1: "Real Betis", equipo2: "Elche C.F.", goles1: 2, goles2: 1 },
+  { equipo1: "Celta de Vigo", equipo2: "Levante U.D.", goles1: 2, goles2: 3 },
   { equipo1: "Getafe C.F.", equipo2: "R.C.D. Mallorca", goles1: null, goles2: null },
   { equipo1: "Girona F.C.", equipo2: "Real Sociedad", goles1: null, goles2: null },
   { equipo1: "R.C.D. Espanyol", equipo2: "Athletic Club", goles1: null, goles2: null },
-  { equipo1: "C.A. Osasuna", equipo2: "Atlético de Madrid", goles1: null, goles2: null },
+  { equipo1: "C.A. Osasuna", equipo2: "Atlético de Madrid", goles1: 1, goles2: 2 },
   { equipo1: "Valencia C.F.", equipo2: "Rayo Vallecano", goles1: null, goles2: null },
   { equipo1: "Villarreal C.F.", equipo2: "Sevilla F.C.", goles1: null, goles2: null },
   { equipo1: "Real Madrid", equipo2: "Real Oviedo", goles1: null, goles2: null },
