4716ff6538e12f571ccf0e37e93cc86c8d43b0b1
taramunde <taramunde1974@gmail.com>
2026-05-16 10:05:38 +0200

    ng

--- js/news-data.js
+++ js/news-data.js
@@ -4,34 +4,34 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/15Qs5NRP/logo_-_La_Voz_de_Asturias.webp",
         sourceName: "La Voz de Asturias",
-        image: "https://i.postimg.cc/CMsp1rGy/J-35-LVA.jpg", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
-        title: "Un digno Oviedo evita ante el Getafe el descenso matemático.",
+        image: "https://i.postimg.cc/SNSn98z4/J-36-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
+        title: "El Oviedo no es capaz de darse una alegría ante un Madrid en crisis.",
         author: "PABLO FERNÁNDEZ",
-        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/05/10/digno-oviedo-evita-ante-getafe-descenso-matematico/00031778434275452249405.htm"
+        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/05/14/rm-ovi/00031778790904971560135.htm"
     },
     {
         sourceLogo: "https://i.postimg.cc/nV7nJ1J0/logo_-_La_Nueva_España.webp",
         sourceName: "La Nueva España",
-        image: "https://i.postimg.cc/xj3VCpKs/J-35-LNE.webp", // FOTO NOTICIA 2
-        title: "Un orgulloso Oviedo evita el descenso en el Tartiere: empate con dos menos ante el Getafe (0-0).",
+        image: "https://i.postimg.cc/sDjMWSZs/J-36-LNE.jpg", // FOTO NOTICIA 2
+        title: "Entre la bronca a Mbappé y la ovación a Cazorla, otra derrota del Oviedo (2-0).",
         author: "NACHO AZPARREN",
-        link: "https://www.lne.es/real-oviedo/2026/05/10/orgulloso-oviedo-evita-descenso-tartiere-130070510.html"
+        link: "https://www.lne.es/real-oviedo/2026/05/14/bronca-mbappe-ovacion-cazorla-derrota-130247321.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/FRkN1yJF/logo_-_El_Comercio.webp",
         sourceName: "El Comercio",
-        image: "https://i.postimg.cc/ZKYtyPcw/J-35-EC.jpg", // FOTO NOTICIA 3
-        title: "Punto de coraje para prolongar la agonía del Real Oviedo.",
+        image: "https://i.postimg.cc/BQSjFD1s/J-36-EC.webp", // FOTO NOTICIA 3
+        title: "El Real Oviedo no puede con un Real Madrid en crisis.",
         author: "RAMÓN JULIO",
-        link: "https://www.elcomercio.es/real-oviedo/punto-coraje-prolongar-agonia-real-oviedo-20260510211128-nt.html"
+        link: "https://www.elcomercio.es/real-oviedo/real-madrid-oviedo-crisis-cazorla-almada-partido-20260514235219-nt.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/7hvDz5g3/logo_-_Killer_Asturias.webp",
         sourceName: "Killer Asturias",
-        image: "https://i.postimg.cc/RVC5n1dV/J-35-KA.jpg", // FOTO NOTICIA 4
-        title: "La Crónica: El Oviedo resiste al VAR y al Getafe.",
+        image: "https://i.postimg.cc/DyvSGsb7/J-36-KA.jpg", // FOTO NOTICIA 4
+        title: "La Crónica: El Madrid cumple el trámite ante un digno Oviedo.",
         author: "JUAN MORENO",
-        link: "https://killerasturias.com/index.php/cronicas/la-cronica-el-oviedo-resiste-al-var-y-al-getafe"
+        link: "https://killerasturias.com/cronicas/la-cronica-el-madrid-cumple-el-tramite-ante-un-digno-oviedo"
     }
 ];
 
