9108703ad5963c182b6e1e499c47559f3115489e
taramunde <taramunde1974@gmail.com>
2026-05-18 07:24:40 +0200

    cc

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -460,16 +460,16 @@ const enfrentamientos = [
   { equipo1: "Real Madrid", equipo2: "Real Oviedo", goles1: 2, goles2: 0 },
   
   // Jornada 37
-  { equipo1: "Athletic Club", equipo2: "Celta de Vigo", goles1: null, goles2: null },
-  { equipo1: "Atlético de Madrid", equipo2: "Girona F.C.", goles1: null, goles2: null },
-  { equipo1: "F.C. Barcelona", equipo2: "Real Betis", goles1: null, goles2: null },
-  { equipo1: "Elche C.F.", equipo2: "Getafe C.F.", goles1: null, goles2: null },
-  { equipo1: "Levante U.D.", equipo2: "R.C.D. Mallorca", goles1: null, goles2: null },
-  { equipo1: "Rayo Vallecano", equipo2: "Villarreal C.F.", goles1: null, goles2: null },
-  { equipo1: "Real Sociedad", equipo2: "Valencia C.F.", goles1: null, goles2: null },
-  { equipo1: "Real Oviedo", equipo2: "Deportivo Alavés", goles1: null, goles2: null },
-  { equipo1: "C.A. Osasuna", equipo2: "R.C.D. Espanyol", goles1: null, goles2: null },
-  { equipo1: "Sevilla F.C.", equipo2: "Real Madrid", goles1: null, goles2: null },
+  { equipo1: "Athletic Club", equipo2: "Celta de Vigo", goles1: 1, goles2: 1 },
+  { equipo1: "Atlético de Madrid", equipo2: "Girona F.C.", goles1: 1, goles2: 0 },
+  { equipo1: "F.C. Barcelona", equipo2: "Real Betis", goles1: 3, goles2: 1 },
+  { equipo1: "Elche C.F.", equipo2: "Getafe C.F.", goles1: 1, goles2: 0 },
+  { equipo1: "Levante U.D.", equipo2: "R.C.D. Mallorca", goles1: 2, goles2: 0 },
+  { equipo1: "Rayo Vallecano", equipo2: "Villarreal C.F.", goles1: 2, goles2: 0 },
+  { equipo1: "Real Sociedad", equipo2: "Valencia C.F.", goles1: 3, goles2: 4 },
+  { equipo1: "Real Oviedo", equipo2: "Deportivo Alavés", goles1: 0, goles2: 1 },
+  { equipo1: "C.A. Osasuna", equipo2: "R.C.D. Espanyol", goles1: 1, goles2: 2 },
+  { equipo1: "Sevilla F.C.", equipo2: "Real Madrid", goles1: 0, goles2: 1 },
   
   // Jornada 38
   { equipo1: "Deportivo Alavés", equipo2: "Rayo Vallecano", goles1: null, goles2: null },
