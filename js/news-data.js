12637a1c9e6aa4320baac5f8e88b2f58f976301d
taramunde <taramunde1974@gmail.com>
2026-05-18 07:18:59 +0200

    k

--- js/news-data.js
+++ js/news-data.js
@@ -4,34 +4,34 @@ const newsData = [
     {
         sourceLogo: "https://i.postimg.cc/15Qs5NRP/logo_-_La_Voz_de_Asturias.webp",
         sourceName: "La Voz de Asturias",
-        image: "https://i.postimg.cc/SNSn98z4/J-36-LVA.webp", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
-        title: "El Oviedo no es capaz de darse una alegría ante un Madrid en crisis.",
+        image: "https://i.postimg.cc/s2qdz9L7/J-37-LVA.jpg", // ¡PON AQUÍ LA URL DE LA FOTO DE LA NOTICIA!
+        title: "El Tartiere clama por la continuidad de Cazorla y el Alavés se salva.",
         author: "PABLO FERNÁNDEZ",
-        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/05/14/rm-ovi/00031778790904971560135.htm"
+        link: "https://www.lavozdeasturias.es/noticia/azulcarbayon/2026/05/17/tartiere-clama-continuidad-cazorla-alaves-salva/00031779040744849363977.htm"
     },
     {
         sourceLogo: "https://i.postimg.cc/nV7nJ1J0/logo_-_La_Nueva_España.webp",
         sourceName: "La Nueva España",
-        image: "https://i.postimg.cc/sDjMWSZs/J-36-LNE.jpg", // FOTO NOTICIA 2
-        title: "Entre la bronca a Mbappé y la ovación a Cazorla, otra derrota del Oviedo (2-0).",
+        image: "https://i.postimg.cc/tgwQbtc3/J-37-LNE.jpg", // FOTO NOTICIA 2
+        title: "Triste colofón a una temporada para el olvido: derrota del Oviedo ante el Alavés (0-1).",
         author: "NACHO AZPARREN",
-        link: "https://www.lne.es/real-oviedo/2026/05/14/bronca-mbappe-ovacion-cazorla-derrota-130247321.html"
+        link: "https://www.lne.es/real-oviedo/2026/05/17/triste-colofon-temporada-olvido-derrota-130333595.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/FRkN1yJF/logo_-_El_Comercio.webp",
         sourceName: "El Comercio",
-        image: "https://i.postimg.cc/BQSjFD1s/J-36-EC.webp", // FOTO NOTICIA 3
-        title: "El Real Oviedo no puede con un Real Madrid en crisis.",
+        image: "https://i.postimg.cc/tgwQbtcF/J-37-EC.jpg", // FOTO NOTICIA 3
+        title: "El Tartiere clama a Cazorla que se quede en el Real Oviedo.",
         author: "RAMÓN JULIO",
-        link: "https://www.elcomercio.es/real-oviedo/real-madrid-oviedo-crisis-cazorla-almada-partido-20260514235219-nt.html"
+        link: "https://www.elcomercio.es/real-oviedo/tartiere-clama-cazorla-quede-real-oviedo-20260517215639-nt.html"
     },
     {
         sourceLogo: "https://i.postimg.cc/7hvDz5g3/logo_-_Killer_Asturias.webp",
         sourceName: "Killer Asturias",
-        image: "https://i.postimg.cc/DyvSGsb7/J-36-KA.jpg", // FOTO NOTICIA 4
-        title: "La Crónica: El Madrid cumple el trámite ante un digno Oviedo.",
+        image: "https://i.postimg.cc/rp3XTCHS/J-37-KA.jpg", // FOTO NOTICIA 4
+        title: "La Crónica: El Oviedo no encuentra ni consuelo en el Tartiere.",
         author: "JUAN MORENO",
-        link: "https://killerasturias.com/cronicas/la-cronica-el-madrid-cumple-el-tramite-ante-un-digno-oviedo"
+        link: "https://killerasturias.com/cronicas/la-cronica-el-oviedo-no-encuentra-ni-consuelo-en-el-tartiere"
     }
 ];
 
